f8f3395825cf09728cf9ff06069229f21211ccd7
LeangHuy <120023658+LeangHuy@users.noreply.github.com>
2024-08-09 10:42:52 +0700

    update openApiDefinition

--- src/main/java/com/config/practiceentitymanager/PracticeEntityManagerApplication.java
+++ src/main/java/com/config/practiceentitymanager/PracticeEntityManagerApplication.java
@@ -1,9 +1,14 @@
 package com.config.practiceentitymanager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Homework-entity-manager",
+        version = "v1",
+        description = "Homework EntityManager"))
 public class PracticeEntityManagerApplication {
 
     public static void main(String[] args) {
